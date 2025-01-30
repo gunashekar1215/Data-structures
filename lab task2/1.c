01e60d7e4f1cefa393b912ff44a02ad5f8896a84
gunashekar1215 <gunashekarcsk@gmail.com>
2025-01-30 10:33:55 +0530

    Update 1.c

--- lab task2/1.c
+++ lab task2/1.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+
+void moving_average(int arr[], int n, int window_size) {
+    for (int i = 0; i <= n - window_size; i++) {
+        float sum = 0;
+        for (int j = 0; j < window_size; j++) {
+            sum += arr[i + j];
+        }
+        printf("%.2f ", sum / window_size);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int window_size = 3;
+    moving_average(arr, n, window_size);
+    return 0;
+}
+
 
