365cb47b3c3a61c68ff065bb72edf576ce891383
gunashekar1215 <gunashekarcsk@gmail.com>
2025-01-30 10:37:06 +0530

    Update 7.c

--- lab task2/7.c
+++ lab task2/7.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int find_kth_largest(int arr[], int n, int k) {
+    for (int i = 0; i < k; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] < arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr[k - 1];
+}
+
+int main() {
+    int arr[] = {3, 2, 1, 5, 6, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = 2;
+    printf("k-th largest: %d\n", find_kth_largest(arr, n, k));
+    return 0;
+}
