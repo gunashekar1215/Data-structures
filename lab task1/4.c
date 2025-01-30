2a2abd41c2ba6f7de6e4b911a1d74d6c8e04d81b
gunashekar1215 <gunashekarcsk@gmail.com>
2025-01-30 10:30:18 +0530

    Update 4.c

--- lab task1/4.c
+++ lab task1/4.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+void reverse_array(int arr[], int n) {
+    int start = 0, end = n - 1;
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    reverse_array(arr, n);
+    printf("Reversed array: ");
+    traverse_array(arr, n);
+
+    return 0;
+}
