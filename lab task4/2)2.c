1c9dfe1e7248a5d03e6d9b820880bc93c3097eff
gunashekar1215 <gunashekarcsk@gmail.com>
2025-02-20 09:21:20 +0530

    Create 2)2.c

---
+++ lab task4/2)2.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (!newNode) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertAtEnd(Node** head, int data) {
+    Node* newNode = createNode(data);
+    
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void displayLinkedList(Node* head) {
+    if (head == NULL) {
+        printf("The linked list is empty.\n");
+        return;
+    }
+    
+    printf("Linked List: ");
+    Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    Node* head = NULL;
+    int n, data;
+
+    printf("Enter the number of nodes to insert at the end: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter data for node %d: ", i + 1);
+        scanf("%d", &data);
+        insertAtEnd(&head, data);
+        displayLinkedList(head);
+    }
+
+    return 0;
+}
